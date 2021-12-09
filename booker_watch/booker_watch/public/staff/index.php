da902d52264e1f5c96ffaa5b75f3671ceb39d5be
Brook Miller <millerbrook@gmail.com>
2021-12-09 17:30:22 -0500

    Begins developing staff area index file

---
+++ booker_watch/booker_watch/public/staff/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>BookerWatch</title>
+    <meta charset="utf-8">
+  </head>
+
+  <body>
+    <header>
+      <h1>Booker Watch Staff Area</h1>
+    </header>
+
+    <navigation>
+      <ul>
+        <li><a href="index.php">Menu</a></li>
+      </ul>
+    </navigation>
+    <footer>
+      &copy; <?php echo date('Y'); ?> Brook Miller
+    </footer>
+  </body>
+</html>
