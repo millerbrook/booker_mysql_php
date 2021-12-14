770b309587b4aaceeaef712ba7a451513a24d324
Brook Miller <millerbrook@gmail.com>
2021-12-14 18:46:55 -0500

    Adds output buffering

--- booker_watch/booker_watch/private/initialize.php
+++ booker_watch/booker_watch/private/initialize.php
@@ -1,5 +1,7 @@
 <?php 
 
+    ob_start(); //initializes output buffering
+
     //Assign file paths to PHP constants
     // __FILE__ returns the current path to this file
     // dirname() returns the path to the parent directory
@@ -18,5 +20,5 @@
   define("WWW_ROOT", $doc_root);
 
   require_once('functions.php');
-  
+
 ?>
